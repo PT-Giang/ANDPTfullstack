a3dbb7817516f5ee9097742feb50d9ccc18f6c3c
PT-Giang <truonggiang10052k4@gmail.com>
2026-05-25 11:02:57 +0700

    final

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://172.20.10.5:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
